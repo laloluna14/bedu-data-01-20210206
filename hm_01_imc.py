1aa3658cad5674e7df0c29b643f9a4f4dda14f15
Lalo Luna <57363601+laloluna14@users.noreply.github.com>
2021-02-06 20:23:59 -0600

    Jupyter notebooks & Homework

--- hm_01_imc.py
+++ hm_01_imc.py
@@ -1,3 +1,10 @@
 '''
-Program that asks for user's weight and height
+Program that asks for user's weight and height and returns IMC
 '''
+
+weight = float(input('Give me your weight in kilograms: '))
+height = float(input('Give me your height in meters: '))
+
+imc = weight / height ** 2
+
+print(f'Your IMC equals to {imc}')
--- hm_02_tabla.py
+++ hm_02_tabla.py
@@ -1,3 +1,9 @@
 '''
 given a number show its Tabla de Multiplicar
 '''
+
+number = int(input('Give me a number: '))
+
+for i in range(1, 11):
+    mult = number * i
+    print(f'{number} X {i} = {mult}')
